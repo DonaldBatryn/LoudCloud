70021fc9de1bfd1a248d71b27d3fc887faa58c49
Donnie <donald.batryn@gmail.com>
2019-09-06 18:56:00 -0500

    heroku retry

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Loudcloud
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
