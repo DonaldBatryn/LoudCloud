f089c9e4acecd6790ad66af8d180f1f6af9265a1
Donnie <donald.batryn@gmail.com>
2019-09-05 21:09:41 -0500

    updated css

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -23,7 +23,7 @@ export default ({ currentUser, logout, openModal }) => {
         <header className="nav-bar">
             <p><Link to="/home"><img className="logo" src="/assets/cloud.png" alt="LoudCloud" /></Link></p>
             <div className="nav-links"><Link to="/playlists">Playlists</Link></div>
-                <div className="nav-links"><Link to="/songs"><button onClick={() => openModal('Upload')}>Upload</button></Link></div>
+                <div ><Link to="/songs"><button className="nav-links" onClick={() => openModal('Upload')}>Upload</button></Link></div>
             <p className="nav-links">Search:  <input className="search-bar" type="text" spellCheck="false" ></input></p>
             <div  >{display.link1}</div>
             <div  >{display.link2}</div>
